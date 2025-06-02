0360d5089e118f779679bda19725fd36e567eefd
kyoyuncoding <kyohyukyun001@gmail.com>
2025-06-02 22:25:38 +0000

    Editing sidebar (adding logo, changing layout of buttons), making textareas in problemlog dynamicand resized based on content, disabling homescreen animation, adding radius to the text areas across login, register and problemlog pages, setting up the Problems Model to save problems and store them in the log.

--- thinkdev/problemlog/models.py
+++ thinkdev/problemlog/models.py
@@ -3,12 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Problems(models.Model):
-    username = models.CharField(max_length=255, default="")
-    email = models.CharField(max_length=255)
-    problem_title = models.CharField(max_length=255)
-    problem_description = models.CharField(max_length=255) 
-    problem_summary = models.CharField(max_length=255)
-    pseudo_code = models.CharField(max_length=255)
-    source_code = models.CharField(max_length=255)
+    problem_title = models.CharField(max_length=10000)
+    problem_description = models.CharField(max_length=10000) 
+    problem_summary = models.CharField(max_length=10000)
+    pseudo_code = models.CharField(max_length=10000)
+    source_code = models.CharField(max_length=10000)
     submit_time = models.DateTimeField(auto_now_add=True)
 
--- thinkdev/problemlog/views.py
+++ thinkdev/problemlog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Problems
+from datetime import datetime
 
 # Create your views here.
 
@@ -10,7 +12,19 @@ def problem_log(request):
     return render(request, "problemlog.html")
 
 def log(request):
-    return render(request, "log.html")
+
+    if request.method == "POST":
+        if request.POST.get("save_problem_button") and request.POST.get("title_of_problem"):
+            problem_entry = Problems(problem_title = request.POST.get("title_of_problem"), problem_description = request.POST.get("problem_description"), problem_summary = request.POST.get("problem_summary"), pseudo_code = request.POST.get("pseudocode"), source_code = request.POST.get("sourcecode"), submit_time = datetime.now())
+            problem_entry.save()
+        else:
+            return HttpResponse("Ya Need To Enter a Title My Guy")
+
+    problems = Problems.objects.all()
+
+    return render(request, "log.html", {
+        "problems": problems 
+    })
 
 def register(request):
     return render(request, "register.html")
