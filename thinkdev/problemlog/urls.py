2abc07485ff1d80696695a8f92454780db7909ce
kyoyuncoding <kyohyukyun001@gmail.com>
2025-06-08 16:17:52 +0000

    Added editing and deleting entry functionality, standardized the sidebar menu.

--- thinkdev/problemlog/urls.py
+++ thinkdev/problemlog/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("problemlog/", views.problem_log, name="problem_log"),
+    path("problemlog/edit/<int:id_entry>", views.problem_log_edit, name="problem_log_edit"),
     path("register/", views.register, name="register"),
     path("login/", views.login, name="login"),
     path("log/", views.log, name="log")
--- thinkdev/problemlog/views.py
+++ thinkdev/problemlog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Problems
 from datetime import datetime
+from django import forms
+from django.urls import reverse
 
 # Create your views here.
 
@@ -9,30 +11,21 @@ def index(request):
     return render(request, "index.html")
 
 def problem_log(request):
-
-    # Figure out how to use the id of the edited problem.
-    if request.method == "POST":
-        if request.POST.get("edit_button"):
-            entry = Problems.objects.get(id=13)
-            return render(request, "problemlog.html",{
-            "entry": entry
-            })
-
-    return render(request, "problemlog.html")
-
-def log(request):
-
     if request.method == "POST":
         if request.POST.get("save_problem_button") and request.POST.get("title_of_problem"):
             problem_entry = Problems(problem_title = request.POST.get("title_of_problem"), problem_description = request.POST.get("problem_description"), problem_summary = request.POST.get("problem_summary"), pseudo_code = request.POST.get("pseudocode"), source_code = request.POST.get("sourcecode"), submit_time = datetime.now())
             problem_entry.save()
-        else:
+        elif request.POST.get("title_of_problem") == False:
             return HttpResponse("Ya Need To Enter a Title My Guy")
+        return HttpResponseRedirect(reverse("log"))
 
+    return render(request, "problemlog.html")
+
+def log(request):
     problems = Problems.objects.all()
 
     return render(request, "log.html", {
-        "problems": problems 
+    "problems": problems 
     })
 
 def register(request):
@@ -40,3 +33,31 @@ def register(request):
 
 def login(request):
     return render(request, "login.html")
+
+def problem_log_edit(request, id_entry):
+
+    if request.POST.get("delete_button"):
+        entry = Problems.objects.get(id=id_entry)
+        entry.delete()
+        return HttpResponseRedirect(reverse("log"))
+
+    if request.POST.get("save_problem_button"):
+        update_entry = Problems.objects.get(id=id_entry)
+        update_entry.problem_title = request.POST.get("title_of_problem")
+        update_entry.problem_description = request.POST.get("problem_description")
+        update_entry.problem_summary = request.POST.get("problem_summary")
+        update_entry.pseudo_code = request.POST.get("pseudocode")
+        update_entry.source_code = request.POST.get("sourcecode")
+        update_entry.submit_time = datetime.now()
+        update_entry.save()
+        problems = Problems.objects.all()
+        return render(request, "log.html",{
+        "problems": problems
+        })
+
+    entry = Problems.objects.get(id=id_entry)
+    return render(request, "problemlog.html",{
+    "entry": entry
+    })
+
+
